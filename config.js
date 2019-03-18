22524f86e795b6f68e944ffd9fd1c3096a883ae1
Tino Vyatkin <tino@vtkn.io>
2019-03-18 13:51:25 -0400

    switching from config to plugin

--- config.js
+++ config.js
@@ -39,6 +39,7 @@ module.exports = {
     'sonarjs',
     'sort-requires',
     'unicorn',
+    '@destinationstransfers'
   ],
   rules: {
     // code quality
---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  rules: require('./rules'),
+  configs: {
+    recommended: require('./config'),
+  },
+};
